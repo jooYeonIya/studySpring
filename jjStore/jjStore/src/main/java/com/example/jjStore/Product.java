5d47e08a05411973cae6d0399c7029732e4c3ab0
jooyeoniya <jooyeonstudyios@gmail.com>
2024-10-23 12:27:08 +0900

    jjStore: Product model 추가

---
+++ jjStore/jjStore/src/main/java/com/example/jjStore/Product.java
@@ -0,0 +1,22 @@
+package com.example.jjStore;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Product {
+  private int id;
+  private String name;
+  private int price;
+  private String maker;
+  private int stock;
+
+  public Product(int id, String name, int price, String maker, int stock) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.maker = maker;
+    this.stock = stock;
+  }
+}
