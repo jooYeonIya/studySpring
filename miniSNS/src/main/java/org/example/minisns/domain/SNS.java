83fd58d222cb1cfbc1310d561299c5c3bcd5f509
jooyeoniya <jooyeonstudyios@gmail.com>
2024-10-31 12:26:09 +0900

    miniSNS: 첫 빌드 시 에러난 부분 수정하고 domain에 주석 추가

--- miniSNS/src/main/java/org/example/minisns/domain/SNS.java
+++ miniSNS/src/main/java/org/example/minisns/domain/SNS.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
 
+//해당 클래스가 JPA(Entity)를 나타낸다는 것을 의미
 @Entity
 @Table(name="sns")
 @AllArgsConstructor
@@ -22,3 +23,15 @@ public class SNS {
   private String body;
   private int likes;
 }
+
+
+//Hibernate:
+//create table sns (
+//    id varchar(6) not null,
+//body varchar(45),
+//likes integer not null,
+//title varchar(6),
+//primary key (id)
+//    ) engine=InnoDB
+// 빌드 시, sns 테이블이 있는지 확인 후 없으면 만든다
+// 만약에 sns 테이블이 있어도 설정을 update로 했기 때문에 수정 사항이 있으면 업데이트도 해준다
