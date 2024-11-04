ab40cb4260f80ba96a6d96274faa6d83df7e5bac
jooyeoniya <jooyeonstudyios@gmail.com>
2024-11-04 14:35:13 +0900

    template_test: 객체를 써서 꺼내오기

--- template_test/src/main/java/org/example/template_test/controller/TestController.java
+++ template_test/src/main/java/org/example/template_test/controller/TestController.java
@@ -1,5 +1,6 @@
 package org.example.template_test.controller;
 
+import org.example.template_test.domain.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,5 +13,14 @@ public class TestController {
   public String text(Model model) {
     model.addAttribute("data", "hello world");
     return "text"; // templates/text.html로 보내 라는 의미
+  }
+
+  @GetMapping("/student")
+  public String student(Model model) {
+    Student student = new Student();
+    student.setAge(20);
+    student.setName("John");
+    model.addAttribute("data", student);
+    return "student";
   }
 }
---
+++ template_test/src/main/java/org/example/template_test/domain/Student.java
@@ -0,0 +1,9 @@
+package org.example.template_test.domain;
+
+import lombok.Data;
+
+@Data
+public class Student {
+  private String name;
+  private int age;
+}
