93756f6d18ce6a31e080f91afccd3f86fe06f712
jooyeoniya <jooyeonstudyios@gmail.com>
2024-11-04 13:38:56 +0900

    miniSNS: JPAQueryFactory는 @RequiredArgsConstructor로 안되는 거 같다 EntityManager 주입해줘야 함

--- miniSNS/src/main/java/org/example/minisns/user/repository/UserRepositoryCustomImpl.java
+++ miniSNS/src/main/java/org/example/minisns/user/repository/UserRepositoryCustomImpl.java
@@ -1,15 +1,21 @@
 package org.example.minisns.user.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.example.minisns.user.domain.QUser;
 import org.example.minisns.user.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 public class UserRepositoryCustomImpl implements UserRepositoryCustom {
   private final JPAQueryFactory queryFactory;
+
+  @Autowired
+  public UserRepositoryCustomImpl(EntityManager entityManager) {
+    this.queryFactory = new JPAQueryFactory(entityManager);
+  }
 
   @Override
   public List<User> findByUserIdAndUserName(String userId, String userName) {
