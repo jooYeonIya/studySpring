787d99fb30a6441dbc9927115146585860b73259
jooyeoniya <jooyeonstudyios@gmail.com>
2024-11-07 16:18:11 +0900

    minishopping: first init

---
+++ miniShopping/src/main/java/org/example/minishopping/MiniShoppingApplication.java
@@ -0,0 +1,13 @@
+package org.example.minishopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniShoppingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MiniShoppingApplication.class, args);
+  }
+
+}
---
+++ miniShopping/src/test/java/org/example/minishopping/MiniShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.minishopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniShoppingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
