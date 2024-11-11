73dde3f0dda86520611023319d44bea4c272607c
jooyeoniya <jooyeonstudyios@gmail.com>
2024-11-11 11:11:22 +0900

    miniShopping: 전체 멤버 조회 기능 추가

--- miniShopping/src/main/java/org/example/minishopping/controller/MemberController.java
+++ miniShopping/src/main/java/org/example/minishopping/controller/MemberController.java
@@ -2,22 +2,28 @@ package org.example.minishopping.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.minishopping.dto.MemberCreateDto;
+import org.example.minishopping.dto.MemberInquiryDto;
 import org.example.minishopping.service.MemberService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/shopping")
+@RequestMapping("/shopping/member")
 public class MemberController {
   private final MemberService memberService;
 
   // 멤버 생성
-  @PostMapping("/add/member")
+  @PostMapping("/add")
   public void addMember(MemberCreateDto dto) {
     memberService.addMember(dto);
+  }
+
+  // 전체 멤버 조회
+  @GetMapping("/getAll")
+  public List<MemberInquiryDto> getAllMembers() {
+    return memberService.getAllMembers();
   }
 }
