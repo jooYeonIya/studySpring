cab42ffc811cfac73e7f4464a8be417526fd0305
jooyeoniya <jooyeonstudyios@gmail.com>
2024-11-11 11:28:26 +0900

    miniShopping:. product 추가

---
+++ miniShopping/src/main/java/org/example/minishopping/controller/ProductController.java
@@ -0,0 +1,20 @@
+package org.example.minishopping.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.minishopping.dto.ProductCreateDto;
+import org.example.minishopping.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/shopping/product")
+public class ProductController {
+  private final ProductService productService;
+
+  @GetMapping("/add")
+  public void add(ProductCreateDto dto) {
+    productService.addProduct(dto);
+  }
+}
